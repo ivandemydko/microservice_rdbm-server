d7c9ba053cb82b7abb6930919a2ff5d9793ff944
ivandemydko <ivandemydko@gmail.com>
2020-04-22 15:04:49 +0300

    add sql script

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -10,6 +10,21 @@ use oauth2provider;
   'http://localhost:8080/user/profile',    null, 3000, -1, null, 'false');
 
 
+       INSERT INTO oauth_client_details(
+ client_id,                        resource_ids,               client_secret,
+ scope,                            authorized_grant_types,     web_server_redirect_uri,
+ authorities,                      access_token_validity,      refresh_token_validity,
+ additional_information,           autoapprove)
+ VALUES
+ ('microservice_app', null, 'secret',    'READ_ALL_GUESTS,WRITE_GUEST,UPDATE_GUEST', 'client_credentials',
+  null,    null, 3000, -1, null, 'false');
+
+
+       UPDATE oauth_client_details
+  SET client_secret = '$2a$04$0B5fGTAd01TqGqDoQ8W1cO8Hirja6RaG5vyc9MKLU5ls8QyE6UO9y'
+  WHERE client_id = 'microservice_app';
+
+
       UPDATE oauth_client_details
   SET client_secret = '$2a$04$TSCJpsvX4fcJ7yXBLeJrZeePQ2PHlEY/SkfBJsQ3MegUAFM7lW32i'
   WHERE client_id = 'microservice_authorization_code';
